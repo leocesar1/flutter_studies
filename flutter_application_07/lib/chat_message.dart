99c1561c25e4e0e6141bee465121dfaf19d119e6
Leonardo <leocesar.rj@gmail.com>
2021-10-25 14:21:12 -0300

    Closed

--- flutter_application_07/lib/chat_message.dart
+++ flutter_application_07/lib/chat_message.dart
@@ -3,34 +3,75 @@ import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
   // const ChatMessage({ Key? key }) : super(key: key);
-  ChatMessage(this.data);
+  ChatMessage(this.data, this.mine);
 
+  final bool mine;
   DocumentSnapshot data;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      padding: EdgeInsets.symmetric(vertical: 5),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
-              padding: EdgeInsets.all(10),
-              child: CircleAvatar(
-                backgroundImage: NetworkImage(data['senderPhotoUrl']),
-              )),
+          !mine
+              ? Padding(
+                  padding: EdgeInsets.only(right: 5, top: 20),
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(data['senderPhotoUrl']),
+                  ))
+              : Container(
+                  padding: EdgeInsets.only(left: 100),
+                ),
           Expanded(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                data['imgUrl'] != null
-                    ? Image.network(data['imgUrl'])
-                    : Text(
-                        data['text'],
-                        style: TextStyle(fontSize: 16),
-                      ),
-                Text(data['senderName'],
-                    style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold))
-              ]))
+            child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 800),
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 15,
+                  ),
+                  child: Column(
+                      crossAxisAlignment: mine
+                          ? CrossAxisAlignment.end
+                          : CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(data['senderName'],
+                            textAlign: mine ? TextAlign.end : TextAlign.start,
+                            style: TextStyle(
+                                fontSize: 13, fontWeight: FontWeight.bold)),
+                        Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 15),
+                            color: mine ? Colors.blue[50] : Colors.grey[200],
+                            margin: const EdgeInsets.symmetric(vertical: 5),
+                            child: Column(
+                                crossAxisAlignment: mine
+                                    ? CrossAxisAlignment.end
+                                    : CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  data['imgUrl'] != null
+                                      ? Image.network(
+                                          data['imgUrl'],
+                                          width: 500,
+                                        )
+                                      : Text(
+                                          data['text'],
+                                          style: TextStyle(fontSize: 16),
+                                        ),
+                                ])),
+                      ]),
+                )),
+          ),
+          mine
+              ? Padding(
+                  padding: EdgeInsets.all(5),
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(data['senderPhotoUrl']),
+                  ))
+              : Container(
+                  padding: EdgeInsets.only(right: 100),
+                ),
         ],
       ),
     );
--- flutter_application_07/lib/chat_screen.dart
+++ flutter_application_07/lib/chat_screen.dart
@@ -25,10 +25,13 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   void initState() {
-    FirebaseAuth.instance.authStateChanges().listen((user) {
-      _currentUser = user;
-    });
     super.initState();
+
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      setState(() {
+        _currentUser = user;
+      });
+    });
   }
 
   Future<User?> _getUser() async {
@@ -51,8 +54,9 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   void _sendMessage({String? text, Uint8List? imgFile}) async {
+    setState(() {});
     final User? user = await _getUser();
-    print(user);
+    // print(user!.displayName);
 
     if (user == null) {
       final snack = SnackBar(
@@ -71,23 +75,18 @@ class _ChatScreenState extends State<ChatScreen> {
         'senderPhotoUrl': user.photoURL,
         'text': text,
         'imgUrl': imgFile,
+        'time': Timestamp.now(),
       };
     } else {
-      data = {
-        'uid': "asdas das as das d",
-        'senderName': "Leonardo",
-        'senderPhotoUrl':
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/125px-Flag_of_Brazil.svg.png",
-        'text': text,
-        'imgUrl': imgFile,
-      };
+      data = {};
     }
 
     if (imgFile != null) {
       try {
         firebase_storage.Task task = firebase_storage.FirebaseStorage.instance
             .ref()
-            .child(DateTime.now().millisecondsSinceEpoch.toString())
+            .child(
+                '${_currentUser!.uid.toString()}_${DateTime.now().millisecondsSinceEpoch.toString()}')
             .putData(imgFile);
 
         firebase_storage.TaskSnapshot snapshot = await task;
@@ -96,8 +95,9 @@ class _ChatScreenState extends State<ChatScreen> {
         // e.g, e.code == 'canceled'
       }
     }
-    if (user != null)
-      FirebaseFirestore.instance.collection('messages').add(data);
+    user != null
+        ? FirebaseFirestore.instance.collection('messages').add(data)
+        : null;
   }
 
   @override
@@ -105,8 +105,44 @@ class _ChatScreenState extends State<ChatScreen> {
     return Scaffold(
         key: _scaffoldKey,
         appBar: AppBar(
-          title: Text("Chat"),
+          shadowColor: Colors.black,
+          centerTitle: true,
+          title: Text(_currentUser != null
+              ? 'Olá, ${_currentUser!.displayName}'
+              : 'Chat app'),
           elevation: 0,
+          actions: <Widget>[
+            _currentUser != null
+                ? IconButton(
+                    icon: Icon(Icons.exit_to_app),
+                    onPressed: () {
+                      FirebaseAuth.instance.signOut();
+                      googleSignIn.signOut();
+                      final snack = SnackBar(
+                          content: Text('Você foi desconectado!'),
+                          duration: Duration(seconds: 2));
+
+                      ScaffoldMessenger.of(context).showSnackBar(snack);
+                    })
+                : IconButton(
+                    icon: Icon(Icons.login_sharp),
+                    onPressed: () {
+                      if (_getUser() == null) {
+                        final snack = SnackBar(
+                            content:
+                                Text('Não foi possível realizar a conexão!'),
+                            backgroundColor: Colors.red,
+                            duration: Duration(seconds: 4));
+                        ScaffoldMessenger.of(context).showSnackBar(snack);
+                      } else {
+                        final snack = SnackBar(
+                            content: Text('Conectado!!!'),
+                            backgroundColor: Colors.green,
+                            duration: Duration(seconds: 4));
+                        ScaffoldMessenger.of(context).showSnackBar(snack);
+                      }
+                    })
+          ],
         ),
         body: Column(
           children: <Widget>[
@@ -114,6 +150,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 child: StreamBuilder<QuerySnapshot>(
                     stream: FirebaseFirestore.instance
                         .collection('messages')
+                        .orderBy('time')
                         .snapshots(),
                     builder: (context, snapshot) {
                       switch (snapshot.connectionState) {
@@ -121,18 +158,37 @@ class _ChatScreenState extends State<ChatScreen> {
                         case ConnectionState.waiting:
                           return Center(child: CircularProgressIndicator());
                         default:
-                          List<DocumentSnapshot> documents =
-                              snapshot.data!.docs.reversed.toList();
+                          try {
+                            List<DocumentSnapshot> documents =
+                                snapshot.data!.docs.reversed.toList();
 
-                          return ListView.builder(
-                            itemBuilder: (context, index) {
-                              // print(documents[index].data);
-                              // print(json.decode(documents[index].toString()));
-                              return ChatMessage(documents[index]);
-                            },
-                            itemCount: documents.length,
-                            // reverse: true
-                          );
+                            return ListView.builder(
+                                itemBuilder: (context, index) {
+                                  // print(documents[index].data);
+                                  // print(json.decode(documents[index].toString()));
+                                  return ChatMessage(
+                                      documents[index],
+                                      documents[index]['uid'] ==
+                                          _currentUser?.uid);
+                                },
+                                itemCount: documents.length,
+                                reverse: true);
+                          } catch (e) {
+                            return Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.block,
+                                    size: 150,
+                                    color: Colors.blue[300],
+                                  ),
+                                  Text(
+                                    'Você não está conectado.',
+                                    style: TextStyle(fontSize: 30),
+                                  ),
+                                ]);
+                          }
                       }
                     })),
             TextComposer(_sendMessage)
